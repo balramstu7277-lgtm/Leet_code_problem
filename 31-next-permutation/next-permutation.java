0ba7c935657d5f050512ac2063b12e8a2799130e
Balram Kumar Rana <balramstu7277@gmail.com>
2026-08-23 22:56:47 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (70.44%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,32 +1,43 @@
-class Solution {
-    public void Swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+class Solution { 
 
-    public void nextPermutation(int[] nums) {
-        int i = nums.length - 2;
-        if(nums.length <=1){
-            return;
-        }
+    public void Swap(int[] arr, int i, int j) { 
+        int temp = arr[i]; 
+        arr[i] = arr[j]; 
+        arr[j] = temp; 
+    } 
+ 
+    public void nextPermutation(int[] nums) { 
+        int i = nums.length - 2; 
 
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-        if (i >= 0) {
-            int j = nums.length - 1;
-            while (nums[i] >= nums[j]) {
-                j--;
-            }
-            Swap(nums, i, j);
-        }
-        int j = nums.length - 1;
+        // If array has 0 or 1 element
+        if(nums.length <= 1){ 
+            return; 
+        } 
+ 
+        // Find the first decreasing element from right
+        while (i >= 0 && nums[i] >= nums[i + 1]) { 
+            i--; 
+        } 
 
-        while (i + 1 < j) {
-            Swap(nums, i + 1, j);
-            i++;
-            j--;
-        }
-    }
+        if (i >= 0) { 
+            int j = nums.length - 1; 
+
+            // Find element greater than nums[i]
+            while (nums[i] >= nums[j]) { 
+                j--; 
+            } 
+
+            // Swap pivot and next greater element
+            Swap(nums, i, j); 
+        } 
+
+        int j = nums.length - 1; 
+ 
+        // Reverse the remaining elements
+        while (i + 1 < j) { 
+            Swap(nums, i + 1, j); 
+            i++;  
+            j--; 
+        } 
+    } 
 }
